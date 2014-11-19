d679fdfe44e84a3fd2583f64a1e3d2c167b31311
wfsiew <siewwingfei@hotmail.com>
2014-11-19 17:32:22 +0800

    a

--- app/__init__.py
+++ app/__init__.py
@@ -6,4 +6,6 @@ Created on 19 Nov 2014
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object('config')
+
 from app import views
--- app/views.py
+++ app/views.py
@@ -3,8 +3,9 @@ Created on 19 Nov 2014
 
 @author: wingfei.seiw
 '''
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -24,3 +25,14 @@ def index():
                            title='Home',
                            user=user,
                            posts=posts)
+    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html', 
+                           title='Sign In',
+                           form=form)
